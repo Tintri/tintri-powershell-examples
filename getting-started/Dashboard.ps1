9375433b31a3d40343a8382e89c3f6708fe1a8b9
Rick Ehrhart <rick_ehrhart@yahoo.com>
2015-02-02 00:44:51 -0800

    Dashboard and Datastore Statistics
    Simple example on how to use the PowerShell cmdlets Get-TintriDashboard
    and Get-TintriDatastoreStat.

---
+++ getting-started/Dashboard.ps1
@@ -0,0 +1,42 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Tintri, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# This script demonstrates getting the dashboard
+# and datastore stats using Tintri powershell cmdlets.
+
+Param(
+  [string] $tintriserver,
+  [string] $tsusername,
+  [string] $tspassword
+)
+
+ipmo 'C:\Program Files\TintriPSToolkit\TintriPSToolkit.psd1'
+
+Connect-TintriServer -Server $tintriserver -UserName $tsusername -Password $tspassword
+
+Write-Output "Dashboard Info:`n"
+
+Get-TintriDashboard
+
+Write-Output "Datastore Stats:`n"
+
+Get-TintriDatastoreStat
