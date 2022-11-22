3257d370715807571986e56e38833c408d08eb2c
Christopher J Rood <61169695+cjrood@users.noreply.github.com>
2022-11-22 15:11:49 -0800

    Add files via upload
    Changes for powershell 7 and .net core to Tintri PSTK.

--- SystemCenterOrchestrator/OrchestratorSyncVMExample.ps1
+++ SystemCenterOrchestrator/OrchestratorSyncVMExample.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 The MIT License (MIT)
 
-Copyright (c) 2017 Tintri, Inc.
+Copyright © 2022 Tintri by DDN, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 #>
 
 <#
@@ -48,8 +47,9 @@ SOFTWARE.
 $standalone = $true
 
 # environment-specific constants defined here.
-$DeveloperVMName = "vmlevel-app-dev1"
+
 $ProductionVMName = "vmlevel-app-prod"
+$DeveloperVMName = "vmlevel-app-dev1"
 $TintriVMstoreName = "vmstore01.vmlevel.com"
 
 if(-not $standalone) {
@@ -61,6 +61,7 @@ if(-not $standalone) {
     # Use pre-defined test constant when running under ISE.
     $param = $DeveloperVMName
 }
+if ($PSEdition -ne "Core") { $tpsEdition = "" } else { $tpsEdition = $PSEdition }
 
 
 # Variables to hold status information to be returned
@@ -73,6 +74,7 @@ $ArgsList = @()
 $ArgsList += $param
 $ArgsList += $ProductionVMName
 $ArgsList += $TintriVMstoreName
+$ArgsList += $tpsEdition
 
 # Orchestrator runs 32-bit PowerShell 2.0, which isn't good for a lot in this
 # modern age. We'll start a new PowerShell session as a workaround. Once we're
@@ -82,7 +84,7 @@ $ArgsList += $TintriVMstoreName
 # Under PowerShell ISE, this will bring up a dialogue box prompting for a set
 # of credentials. This doesn't happen under Orchestrator, but in the ISE case,
 # you can add the -EnableNetworkAccess option to have SSO work.
-$Session = New-PSSession -ComputerName localhost
+$Session = New-PSSession -ComputerName $ENV:COMPUTERNAME
 
 $TraceLog += "{$(Get-Date -Format o)}: Starting PowerShell Session`r`n"
 
@@ -97,7 +99,8 @@ $Results = Invoke-Command -Session $Session -ArgumentList $ArgsList -ScriptBlock
     Param(
         [ValidateNotNullOrEmpty()][string]$VMname,
         [ValidateNotNullOrEmpty()][string]$ProdVMName,
-        [ValidateNotNullOrEmpty()][string]$VMstore
+        [ValidateNotNullOrEmpty()][string]$VMstore,
+        [ValidateNotNullOrEmpty()][string]$tpsEdition
     )
 
     # variables returned below
@@ -114,13 +117,24 @@ $Results = Invoke-Command -Session $Session -ArgumentList $ArgsList -ScriptBlock
         $script:trace += "{$time}: $msg`r`n"
     }
 
+	function filterDiskList( $disks, $includeBootDisk, $description )
+	{
+		$disks = $disks |  sort -property Name | where {(! $_.IsSynthetic) -and ($_.IsDiskdrive) }
+		if ($includeBootDisk -eq $false)
+		{
+			$disks = $disks |  sort -property Name | where { ($_.Name -notlike "* 0:0") }
+		}
+		return $disks
+	}
+
     Write-Trace "Inside Advanced PowerShell Session"
 
     # Here's where we start doing Tintri-specifics
     # First, we load the Tintri Automation Toolkit
     try {
-        Write-Trace "Importing Tintri Automation Toolkit for PowerShell"
-        Import-Module "${ENV:ProgramFiles}\TintriPSToolkit\TintriPSToolkit.psd1"
+		# import the tintri toolkit 
+		Write-Trace "Import the Tintri Powershell Toolkit module (TintriPS$($tpsEdition)Toolkit)."
+		Import-Module -force "${ENV:ProgramFiles}\TintriPS$($tpsEdition)Toolkit\TintriPS$($tpsEdition)Toolkit.psd1"		
     } catch {
         # Store Error status information to be returned to Orchestrator
         $e = $error[0].Exception.Message
@@ -159,10 +173,19 @@ $Results = Invoke-Command -Session $Session -ArgumentList $ArgsList -ScriptBlock
         try {
             Write-Trace "Retrieving production VM ($ProdVMName) object"
             $prodvm = Get-TintriVM -TintriServer $ts -Name $ProdVMName
-            Write-Trace "Retrieving snapshot object for last production snapshot"
+            
+			Write-Trace "Retrieving snapshot for last production snapshot on VM: $ProdVMName"
             $prodsnap = Get-TintriVMSnapshot -GetLatestSnapshot -VM $prodvm
-            Write-Trace "Retrieving disk objects array from snapshot"
-            $proddisks = Get-TintriVDisk -Snapshot $prodsnap
+			
+			$snapDescription = "$($prodsnap.Consistency) $($prodsnap.CreateTime) $($prodsnap.Description)"
+            Write-Trace "Retrieving vdisks from snapshot [$snapDescription] on VM: $ProdVMName"
+            $snapDisks = Get-TintriVDisk -Snapshot $prodsnap 
+			
+            Write-Trace "Retrieving vdisks from target development VM: $VMname"
+			$vmDisks = Get-TintriVDisk -VM $DevVM
+			
+			$snapDisks = filterDiskList $snapDisks $false "Snapshot VM Source"
+			$vmDisks = filterDiskList $vmDisks $false "VM Destination"
         } catch {
             $e = $error[0].Exception.Message
             Write-Trace "Failed to retrieve VM information for ${$VMname}: $e"
@@ -174,9 +197,14 @@ $Results = Invoke-Command -Session $Session -ArgumentList $ArgsList -ScriptBlock
     # And we finally synchronise the second and third virtual disks
     if($status) {
         try {
-            Write-Trace "Syncronising virtual disks"
-            $sync = Sync-TintriVDisk -VM $DevVM -SourceSnapshot $prodsnap -VDiskMap @($null, $proddisks[1], $proddisks[2]) -Force
-            if($sync.State -ne 'SUCCESS') {
+			$snapDisksName = $snapDisks | Select-Object Name, Path, VmName | out-string
+			$vmDisksName = $vmDisks | Select-Object Name, Path, VmName | out-string
+			
+            Write-Trace "Syncronising virtual disks to VM: $VMname"
+            Write-Trace " snapshot disks $snapDisksName."
+            Write-Trace " vm disks $vmDisksName."
+            $sync = Sync-TintriVDisk -VM $DevVM -SourceSnapshot $prodsnap -SnapshotDisk $snapDisks -VMDisk $vmDisks -Force
+            if ($sync.State -ne 'SUCCESS') {
                 throw "Sync returned ${$sync.State}"
             }
         } catch {
@@ -189,7 +217,7 @@ $Results = Invoke-Command -Session $Session -ArgumentList $ArgsList -ScriptBlock
     
     # Close the session
     Write-Trace "Disconnecting session to Tintri VMstore"
-    Disconnect-TintriServer -TintriServer $ts
+    if ($ts) { Disconnect-TintriServer -TintriServer $ts }
 
     # End of Tintri-specifics
 
